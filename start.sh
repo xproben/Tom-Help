42bb8a55bf8d8f9c13d9a8f2da93a0c111e9e543
GRoot <124968285+xproben@users.noreply.github.com>
2023-02-24 05:55:08 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  gh repo clone matrixbotz0/PETER-PARKER-BOT
+  gh repo clone xproben/PETER-PARKER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PETER-PARKER-BOT
